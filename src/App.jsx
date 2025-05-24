a7c9e127fc685f79c52a0574db5b7e954e45bb42
biruk <biruk_ketema@yahoo.com>
2025-05-23 19:14:22 -0600

    catagory and carousel effect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import CarouselEffect from './components/Carousel/CarouselEffect'
+import Catagory from './components/Catagory/Catagory'
 function App() {
   return (
     <div>
-      
+     <Header/> 
+     <CarouselEffect/>
+     <Catagory/>
     </div>
   )
 }
---
+++ src/components/Carousel/Data.js
@@ -0,0 +1,8 @@
+import image1 from "./Images/image 1.png";
+import image2 from "./Images/image 2.png";
+import image3 from "./Images/image 3.png";
+import image4 from "./Images/image 4.png";
+import image5 from "./Images/image 5.png";
+import image6 from "./Images/image 6.png";
+let data = [image1, image2, image3, image4, image5, image6];
+export default data;
